b50e5d2e5cd7910f2dde7be6e01abdbf9466e61f
Mikel Ward <mikel@mikelward.com>
2022-09-11 17:40:42 +0100

    Use a custom date format

--- app/index.js
+++ app/index.js
@@ -8,10 +8,35 @@ clock.granularity = "minutes";
 const myTime = document.getElementById("myTime");
 const myDate = document.getElementById("myDate");
 
+const dayName = {
+    0: 'Sun',
+    1: 'Mon',
+    2: 'Tue',
+    3: 'Wed',
+    4: 'Thu',
+    5: 'Fri',
+    6: 'Sat',
+};
+
+const monthName = {
+    0: 'Jan',
+    1: 'Feb',
+    2: 'Mar',
+    3: 'Apr',
+    4: 'May',
+    5: 'Jun',
+    6: 'Jul',
+    7: 'Aug',
+    8: 'Sep',
+    9: 'Oct',
+    10: 'Nov',
+    11: 'Dec',
+};
+
 clock.ontick = (evt) => {
   let now = evt.date;
   let hours = util.zeroPad(now.getHours());
   let mins = util.zeroPad(now.getMinutes());
   myTime.text = `${hours}:${mins}`;
-  myDate.text = now.toLocaleDateString('en-US');
+  myDate.text = `${dayName[now.getDay()]} ${monthName[now.getMonth()]} ${now.getDate()}`;
 }
